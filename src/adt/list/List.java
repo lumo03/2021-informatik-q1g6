1bc0c8f89e1cdf65f087d6304c8a66440747aa47
lumo03 <61543862+lumo03@users.noreply.github.com>
2022-06-02 20:19:19 +0200

    added relative getter

--- src/adt/list/List.java
+++ src/adt/list/List.java
@@ -25,4 +25,26 @@ public class List<T> extends Queue<T> {
 
         return runner.getData();
     }
+
+    public T getRel(int index) {
+        if (index < 0) {
+            index = getSize() + index;
+            if (index < 0) {
+                throw new IllegalArgumentException("Index must be positive: " + index);
+            }
+        } else if (isEmpty()) {
+            // nicht notwendig, aber verständlichere Fehlermeldung
+            throw new IllegalArgumentException("List is empty.");
+        } else if (index >= getSize()) {
+            throw new IllegalArgumentException("Index is too big.");
+        }
+
+        Item<T> runner = first;
+
+        for (int i = 0; i < index; i++) {
+            runner = runner.getNext();
+        }
+
+        return runner.getData();
+    }
 }
